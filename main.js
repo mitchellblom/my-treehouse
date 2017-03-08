a48d506b3d72d7e6bb887dfcda6835514c9f33fa
Mitchell Blom <mitchellblom@gmail.com>
2017-03-07 18:33:28 -0600

    badges writing to dom

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-// var treehouseContainer = document.getElementById("treehouseContainer");
-
+var treehouseContainer = document.getElementById("treehouseContainer");
+var badgeArray = [];
 
 function executeThisCodeAfterFileLoaded(){
 	var data = JSON.parse(this.responseText);
-	// console.log(data);							/// <-- Uncomment this line to show treehouse content on console
+
+	for (i=0; i < 47; i++) {
+		// console.log(data.badges[i].icon_url);
+		badgeArray += `<img src=" ${data.badges[i].icon_url}" alt="badges-here">;`
+		treehouseContainer.innerHTML = badgeArray;
+		// console.log(badgeArray);			
+	}
+
+	
+
 }
 
 function executeThisCodeAfterFileFails(){
@@ -15,3 +24,10 @@ myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
 myRequest.addEventListener("error", executeThisCodeAfterFileFails);
 myRequest.open("GET", "https://teamtreehouse.com/mitchellblom.json");
 myRequest.send();
+
+
+
+
+//////////
+// For loop
+// CSS - wants it pretty
