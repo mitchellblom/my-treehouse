f9e3d3de725348d962c58a940a2174553facd7a3
Mitchell Blom <mitchellblom@gmail.com>
2017-03-11 16:26:21 -0600

    readme content and minor tidying

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# my-treehouse
+# NSS My Treehouse Exercise
+
+![Splashpage]()
+(I do not own the background image and only claim to use it creatively for practice)
+
+<hr>
+
+## Project Summary / Requirements
+Hour long hackathon to see what each of us could accomplish using the **JSON** scraped from our own Treehouse account profiles. Use of for loops was also required.
+
+<hr>
+
+## Demonstrated
+ - Parsing and manipulating elements of a **JSON** file from the web
+ - **CSS** *transform, z-index, overflow, and position*
+ - Starting with **Bootstrap**
--- main.js
+++ main.js
@@ -2,20 +2,20 @@ var treehouseContainer = document.getElementById("treehouseContainer");
 var profilePic = document.getElementById("profile-pic");
 var badgeArray = [];
 
-function executeThisCodeAfterFileLoaded(){
-	var data = JSON.parse(this.responseText);
+function executeThisCodeAfterFileLoaded() {
+    var data = JSON.parse(this.responseText);
 
-	console.log(data);
-	profilePic.innerHTML = `<img src="${data.gravatar_url}" alt="pic-here">;`
+    console.log(data);
+    profilePic.innerHTML = `<img src="${data.gravatar_url}" alt="pic-here">;`
 
-	for (i=0; i < 47; i++) {
-		badgeArray += `<img src="${data.badges[i].icon_url}" alt="badges-here">;`
-		treehouseContainer.innerHTML = badgeArray;		
-	}
+    for (i = 0; i < 47; i++) {
+        badgeArray += `<img src="${data.badges[i].icon_url}" alt="badges-here">;`
+        treehouseContainer.innerHTML = badgeArray;
+    }
 }
 
-function executeThisCodeAfterFileFails(){
-	// console.log("not working")
+function executeThisCodeAfterFileFails() {
+    // console.log("not working")
 }
 
 var myRequest = new XMLHttpRequest();
@@ -23,10 +23,3 @@ myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
 myRequest.addEventListener("error", executeThisCodeAfterFileFails);
 myRequest.open("GET", "https://teamtreehouse.com/mitchellblom.json");
 myRequest.send();
-
-
-
-
-//////////
-// For loop
-// CSS - wants it pretty
